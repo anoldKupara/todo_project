d0f10f35c3c3bbf2489a8bf99d35888f7d1c8ce7
Anold Kupara <kuparaanold20@gmail.com>
2025-03-24 12:12:09 +0200

    Added the view

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+            return redirect('task_list')
+    return render(request, 'todo/add_task.html')
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
