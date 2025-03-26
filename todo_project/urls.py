33b93fa9debe77be195dd528bc26bc800f37da0b
Anold Kupara <kuparaanold20@gmail.com>
2025-03-26 15:22:06 +0200

    modified the code to redirect to login page upon logging in

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('todo.urls')),  # Add 'tasks/' prefix
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('tasks/', include('todo.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # For login/logout
+    path('', lambda request: redirect('login')),  # Redirect root to login
 ]
